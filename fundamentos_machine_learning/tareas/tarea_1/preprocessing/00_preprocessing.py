e0c14dfb009d451341c4b55be2892f9a9001574d
Gxstalbo <gpvelascop@gmail.com>
2022-10-28 10:23:49 -0300

    primer etl decente despues de limpiar codigo basura

--- fundamentos_machine_learning/tareas/tarea_1/preprocessing/00_preprocessing.py
+++ fundamentos_machine_learning/tareas/tarea_1/preprocessing/00_preprocessing.py
@@ -15,12 +15,9 @@ df_negative= df_sentences.query("CLASS==0")
 del(df_negative['CLASS'])
 
 #vectores_positive= lista_a_matriz([normalizar_vector((vectorizar_frase(frase))) for frase in df_positive['SENTENCE'].tolist()])
-#vectores_positive[-1]
-a=[(vectorizar_frase(frase)) for frase in df_positive['SENTENCE'].tolist()]
+to_large=[(vectorizar_frase(frase)) for frase in df_positive['SENTENCE'].tolist()]
 #Sentencia completa
-largo_maximo= largo_maximo_array(a)
-
-len(a[-1])
+largo_maximo= largo_maximo_array(to_large)
 
 positive_list=[estandarizar_largo(normalizar_vector((vectorizar_frase(frase))),largo_maximo) for frase in df_positive['SENTENCE'].tolist()]
 
@@ -33,31 +30,4 @@ for i in range(len(positive_list)):
 np_positive= lista_a_matriz(positive_list)
 np_positive[-1]
 
-np_positive
-
-len(np_positive)
-
-#bia=[estandarizar_largo(normalizar_vector((vectorizar_frase(frase))),largo_maximo) for frase in df_positive['SENTENCE'].tolist()]
-
-np_positive= lista_a_matriz(positive_list)
-np_positive[-1]
-
-
-for i in range(len(positive_list)):
-    print(i)
-    print(len(positive_list[i]))
-del(positive_list[948])
-
-#b=[normalizar_vector((vectorizar_frase(frase))) for frase in df_positive['SENTENCE'].tolist()]
-b[-1]
-#SC
-#v = a[-1][-1] / np.sum(a[-1][-1])
-max(len(a[-1]))
-type(a)
-type(vectores_positive)
-
-
-df_positive['SENTENCE'].tail(1)
-#for frase in df_positive['SENTENCE'].tolist():
-#    frase.split()
     
